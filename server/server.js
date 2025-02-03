b0dc78e36ea9fb06569be051574f1da4c10d9feb
PaulAyushmaan <ayushmaanpaul2004@gmail.com>
2025-02-03 13:12:42 +0530

    folder structure updated

--- server/server.js
+++ server/server.js
@@ -3,7 +3,7 @@ const cors = require('cors');
 const morgan = require('morgan');
 const dotenv = require('dotenv');
 const colors = require('colors');
-const connectDb = require('./config/connectDb');
+const connectDb = require('./src/config/connectDb');
 dotenv.config();
 const app = express();
 app.use(morgan('dev'));
@@ -11,9 +11,9 @@ app.use(express.json());
 app.use(cors());
 connectDb();
 //user routes
-app.use('/api/v1/users', require('./routes/userRoute'));
+app.use('/api/v1/users', require('./src/routes/userRoute'));
 //transaction routes
-app.use('/api/v1/transactions', require('./routes/transactionRoutes'));
+app.use('/api/v1/transactions', require('./src/routes/transactionRoutes'));
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => {
 	console.log(`Server is running on port ${PORT}`.yellow.bold);
